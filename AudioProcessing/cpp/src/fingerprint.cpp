cdc654becd4f9637e77438e3fd248e28d5266451
abransh <abranshbaliyan2807@gmail.com>
2025-03-11 03:19:36 +0100

    making audioprocessing

--- AudioProcessing/cpp/src/fingerprint.cpp
+++ AudioProcessing/cpp/src/fingerprint.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <fftw3.h>
+
 
 class AudioFingerPrinter {
 
@@ -42,9 +44,37 @@ std::vector<std::vector<float>> computeSpectogram(const std::vector<float> & aud
 {
     int numFrames = (audioData.size() - frameSize) / hopSize + 1; 
     std::vector<std::vector<float>> spectogram(numFrames, std::vector<float>(frameSize/ 2 + 1, 0.0)); 
-    
+ 
+    fftwf_complex* out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex))* (frameSize/ 2 + 1);
+    float* in = (float*) fftwf_malloc(sizeof(float) * frameSize); 
+    fftwf_plan plan = fftwf_plan_dft_r2c_1d(frameSize, in, out, FFTW_ESTIMATE);
+
+    for(int frame = 0; frame < numFrames; frame++)
+    {
+        for (int i = 0; i < frameSize; i++) {
+                if (frame * hopSize + i < audioData.size()) {
+                    float hannWindow = 0.5 * (1 - std::cos(2 * M_PI * i / (frameSize - 1)));
+                    in[i] = audioData[frame * hopSize + i] * hannWindow;
+                } else {
+                    in[i] = 0.0; // 
+                }
+    } 
+
+    ffwtf_execute(p); 
+
+    for (int i= 0; i <= frameSize / 2: i++)
+    {
+        float real = out[i][0]; 
+        float imag  = out[i][1]; 
+          spectogram[frame][i] = std::sqrt(real * real + imag * imag);
+    }
+ 
+
 }
         
 
 
 }
+}
+
+
